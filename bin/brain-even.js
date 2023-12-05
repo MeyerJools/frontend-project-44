d339bb61a171c7d38b67ae7fc828d6bb3994c729
Юлия Мейер <alinasmey2@gmail.com>
2023-12-05 22:13:19 +0500

    make brain-even game

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,7 +1,6 @@
 #!/usr/bin/env node
-import readlineSync from 'readline-sync';
+import brainEven from '../src/brain-even-code.js';
 
 console.log('Welcome to the Brain Games!');
-const name = readlineSync.question('May I have your name? ');
-console.log(`Hello, ${name}!`);
-console.log('Answer "yes" if the number is even, otherwise answer "no".');
+
+brainEven();
