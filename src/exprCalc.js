629cfa77651058a868fcf3ab451fb0124cab06a0
Юлия Мейер <alinasmey2@gmail.com>
2024-02-20 23:02:19 +0500

    fixed calc

---
+++ src/exprCalc.js
@@ -0,0 +1,15 @@
+const calc = (operator, operand1, operand2) => {
+  let result;
+  if (operator === '+') {
+    result = operand1 + operand2;
+  }
+  if (operator === '-') {
+    result = operand1 - operand2;
+  }
+  if (operator === '*') {
+    result = operand1 * operand2;
+  }
+  return result;
+};
+
+export default calc;
--- src/games/brCalc.js
+++ src/games/brCalc.js
@@ -1,28 +1,19 @@
 import playGame from '../index.js';
 import getRandomNumber from '../random.js';
+import calc from '../exprCalc.js';
 
 const playBrainCalc = () => {
   const askQuestion = () => console.log('What is the result of the expression?');
 
   const createQuestionAnswer = () => {
-    const range = [1, 100];
+    const rangeStart = 1;
+    const rangeEnd = 100;
     const operators = ['+', '-', '*'];
-    const operator = operators[getRandomNumber(0, 2)];
-    const operand1 = getRandomNumber(range[0], range[1]);
-    const operand2 = getRandomNumber(range[0], range[1]);
+    const operator = operators[getRandomNumber(0, operators.length - 1)];
+    const operand1 = getRandomNumber(rangeStart, rangeEnd);
+    const operand2 = getRandomNumber(rangeStart, rangeEnd);
     const question = `${operand1} ${operator} ${operand2}`;
-
-    let answer;
-    if (operator === '+') {
-      answer = operand1 + operand2;
-    }
-    if (operator === '-') {
-      answer = operand1 - operand2;
-    }
-    if (operator === '*') {
-      answer = operand1 * operand2;
-    }
-
+    const answer = calc(operator, operand1, operand2);
     return [question, answer.toString()];
   };
 
